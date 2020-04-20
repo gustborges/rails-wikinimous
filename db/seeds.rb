df8c546fbee6d0b5cf8caafdfabd8a56ff1dae81
Gustavo Borges <gustborges@gmail.com>
2020-04-19 21:28:05 -0300

    Created wiki functionality, but it needs some front-end work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence(word_count: 3),
+    content: Faker::Lorem.paragraph(sentence_count: 5)
+  )
+  article.save!
+end
